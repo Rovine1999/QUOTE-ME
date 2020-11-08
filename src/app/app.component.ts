121bf6e8efa2976def315e3eafee44872e527800
Rovine1999 <rovinewanjala99@gmail.com>
2020-11-08 22:15:30 +0300

    add strikethrough.directive.spec.ts and strikethrough.directive.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,5 @@ import { Quote } from './quote';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // quotes: Quote[] = [
-  //   new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
-  //   new Quote(2,'Buy Cookies','I have to buy cookies for the parrot'),
-  //   new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon'),
-  //   new Quote(4,'Get Dog Food','Pupper likes expensive snacks'),
-  //   new Quote(5,'Solve math homework','Damn Math'),
-  //   new Quote(6,'Plot my world domination plan','Cause I am an evil overlord'),
-  // ];
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
