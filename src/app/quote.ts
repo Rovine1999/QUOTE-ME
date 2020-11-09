643cbcbfc2efba447fee9e8a660db54db142df10
Rovine1999 <rovinewanjala99@gmail.com>
2020-11-09 22:07:42 +0300

    added quote-form component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,10 @@
 export class Quote {
   public showDescription: boolean;
+  unlike: number;
+  like: number;
   constructor(public id: number,public name: string,public description: string,public author: string,public completeDate: Date){
     this.showDescription=false;
+    this.unlike = 0;
+    this.like = 0;
   }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,11 +10,19 @@ export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
     new Quote(1, 'Robert Mugabe', 'Love is a scandal of the personal sort.', 'Chekhov', new Date(2020, 5, 6)),
     new Quote(2,'Martin Luther King', 'Man\'s life is brief, but yet there is no man who cannot boast\ that there have been terrible moments in his past.', 'Chekhov', new Date(2016, 5, 6)),
-    new Quote(3,'Joseph-Beep-Tech', "You can't have it all, all at once.", 'Ruth Ginsberg', new Date(2019, 5, 6)),
+    new Quote(3,'Nelson Mandela', "You can't have it all, all at once.", 'Ruth Ginsberg', new Date(2019, 5, 6)),
     new Quote(4, 'Robert Mugabe', "All happy families are alike; each unhappy family is unhappy in its own way.", ' Leo Tolstoy , Anna Karenina', new Date(2018, 5, 6)),
     new Quote(5, 'Robert Mugabe', 'Nothing makes a woman more confused than being in a relation with a broke man who is extremely good in bed', 'Rovine Wanjala', new Date(2010, 7,7)),
     new Quote(6, 'Robert Mugabe', 'Sometimes you look back at girls you spent money on, rather than send it to your Mom and you realise witchcraft is real', 'Ruthov', new Date(2011, 4, 5)),
   ];
+
+  like(index){
+    this.quotes[index].like++;
+  }
+  
+  unlike(index){
+    this.quotes[index].unlike++;
+  }
 
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
@@ -33,7 +41,6 @@ export class QuoteComponent implements OnInit {
       }
     }
   }
-
   
   constructor() { }
 
